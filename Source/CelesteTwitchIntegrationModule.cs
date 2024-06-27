72208054171a79aa55b3286d79aa745f2afb5d81
cpinta <pinta.christian@outlook.com>
2024-06-26 19:01:48 -0500

    Reading and Writing Twitch Messages works.

--- Source/CelesteTwitchIntegrationModule.cs
+++ Source/CelesteTwitchIntegrationModule.cs
@@ -1,5 +1,9 @@
-﻿using MonoMod.ModInterop;
+﻿using Celeste.Mod.Hyperline;
+using MonoMod.ModInterop;
 using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Celeste.Mod.CelesteTwitchIntegration
 {
@@ -16,6 +20,9 @@ namespace Celeste.Mod.CelesteTwitchIntegration
         public override Type SaveDataType => typeof(CelesteTwitchIntegrationModuleSaveData);
         public static CelesteTwitchIntegrationModuleSaveData SaveData => (CelesteTwitchIntegrationModuleSaveData)Instance._SaveData;
 
+        static TcpClient tcpClient = new TcpClient();
+        static StreamReader streamReader;
+        static StreamWriter streamWriter;
 
         public static string ip = "irc.chat.twitch.tv";
         public static int port = 6667;
@@ -23,6 +30,7 @@ namespace Celeste.Mod.CelesteTwitchIntegration
         public static JSONFile iniConfig = new JSONFile(iniPath);
         public static string password;
         public static string botUsername = "pinta_bot";
+        public static string twitchChannelName = "pintalive";
 
         public CelesteTwitchIntegrationModule()
         {
@@ -30,7 +38,7 @@ namespace Celeste.Mod.CelesteTwitchIntegration
 #if DEBUG
             // debug builds use verbose logging
             Logger.SetLogLevel(nameof(CelesteTwitchIntegrationModule), LogLevel.Verbose);
-            
+
 #else
             // release builds use info logging to reduce spam in log files
             Logger.SetLogLevel(nameof(CelesteTwitchIntegrationModule), LogLevel.Info);
@@ -39,11 +47,59 @@ namespace Celeste.Mod.CelesteTwitchIntegration
 
         public override void Load()
         {
+
             typeof(CelesteTwitchIntegrationExports).ModInterop(); // TODO: delete this line if you do not need to export any functions
+            Console.WriteLine("TEST LOG");
 
             password = iniConfig.Get("OAUTH");
+            tcpClient = new TcpClient();
+            tcpClient.Connect(ip, port);
 
+
+            streamReader = new StreamReader(tcpClient.GetStream());
+            streamWriter = new StreamWriter(tcpClient.GetStream()) { NewLine = "\r\n", AutoFlush = true };
+
+            streamWriter.WriteLine($"PASS {password}");
+            streamWriter.WriteLine($"NICK {botUsername}");
+            streamWriter.WriteLine($"JOIN #{twitchChannelName}");
             // TODO: apply any hooks that should always be active
+
+            ReadTwitchChat();
+        }
+
+        public static void GamePausedMessage(Level level, int startIndex, bool minimal, bool quickReset)
+        {
+            SendTwitchMessage($"the game is paused :D");
+        }
+
+        public static void SendTwitchMessage(string message)
+        {
+            streamWriter.WriteLine($"PRIVMSG #{twitchChannelName} :{message}");
+        }
+
+        public async void ReadTwitchChat()
+        {
+            while (true)
+            {
+                //:pintalive!pintalive@pintalive.tmi.twitch.tv PRIVMSG #pintalive :rah
+
+                string line = await streamReader.ReadLineAsync();
+                string[] split = line.Split(':');
+                if (split.Length > 2)
+                {
+                    Console.WriteLine(split[2]);
+
+                    if (split[2].StartsWith("hair"))
+                    {
+                        string[] options = split[2].Split(" ");
+                        if (options[1] == "yellow")
+                        {
+                            Hyperline.Hyperline.Instance.UI.SetHairLength(10, 10);
+                            
+                        }
+                    }
+                }
+            }
         }
 
         public override void Unload()
